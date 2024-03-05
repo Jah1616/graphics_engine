725692cecbebf5bec32cb9c7477736d391c59076
Jahson Kamara <jahson.kamara@gmail.com>
2024-03-05 16:05:56 +0100

    idk

---
+++ cmake-build-debug/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/2D_L-System/l_parser/l_parser.cc.obj"
+  "CMakeFiles/engine.dir/2D_L-System/l_parser/l_parser.cc.obj.d"
+  "CMakeFiles/engine.dir/easy_image.cc.obj"
+  "CMakeFiles/engine.dir/easy_image.cc.obj.d"
+  "CMakeFiles/engine.dir/engine.cc.obj"
+  "CMakeFiles/engine.dir/engine.cc.obj.d"
+  "CMakeFiles/engine.dir/ini_configuration.cc.obj"
+  "CMakeFiles/engine.dir/ini_configuration.cc.obj.d"
+  "engine.exe"
+  "engine.exe.manifest"
+  "engine.pdb"
+  "libengine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
