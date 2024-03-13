5a06cf1d46140aae1d6cef4b22bddd09fc8e72da
Jahson Kamara <jahson.kamara@gmail.com>
2024-03-13 18:51:39 +0100

    3d wireframes finished

--- 3D/Figure3D.hpp
+++ 3D/Figure3D.hpp
@@ -23,7 +23,7 @@ struct Figure3D{
 using Figures3D = std::list<Figure3D>;
 
 void applyTransform(Figure3D& f, const Matrix& m){
-    for (Vector3D point : f.points) point *= m;
+    for (Vector3D& point : f.points) point *= m;
 }
 
 void applyTransform(Figures3D& f, const Matrix& m){
@@ -39,8 +39,7 @@ Matrix scale(const double factor){
     return m;
 }
 
-Matrix rotateX(double angle){
-    angle *= M_PI/180;
+Matrix rotateX(const double angle){
     Matrix m;
     m(1, 1) = 1;
     m(2, 2) = cos(angle);
@@ -51,8 +50,7 @@ Matrix rotateX(double angle){
     return m;
 }
 
-Matrix rotateY(double angle){
-    angle *= M_PI/180;
+Matrix rotateY(const double angle){
     Matrix m;
     m(1, 1) = cos(angle);
     m(1, 3) = -sin(angle);
@@ -63,8 +61,7 @@ Matrix rotateY(double angle){
     return m;
 }
 
-Matrix rotateZ(double angle){
-    angle *= M_PI/180;
+Matrix rotateZ(const double angle){
     Matrix m;
     m(1, 1) = cos(angle);
     m(1, 2) = sin(angle);
@@ -88,9 +85,9 @@ Matrix translate(const Vector3D& vector){
 }
 
 void toPolar(const Vector3D& point, double &theta, double &phi, double &r){
-    r = sqrt(pow(point.x, 2) + pow(point.y, 2) + pow(point.z, 2));
-    theta = atan2(point.y, point.x);
-    phi = acos(point.z / r);
+    r = sqrt(point.x*point.x + point.y*point.y + point.z*point.z);
+    theta = std::atan2(point.y, point.x);
+    phi = std::acos(point.z / r);
 }
 
 Matrix eyePointTrans(const Vector3D& eyepoint){
@@ -135,52 +132,45 @@ img::EasyImage generate3DLinesImage(const ini::Configuration &conf){
     const unsigned int size = conf["General"]["size"].as_int_or_die();
     const std::vector<double> bgColor = conf["General"]["backgroundcolor"].as_double_tuple_or_die();
     const unsigned int nrFigs = conf["General"]["nrFigures"].as_int_or_die();
-    const std::vector<double> eye = conf["General"]["eye"].as_double_tuple_or_die();
-    const Vector3D eyePoint = Vector3D::vector(eye[0], eye[1], eye[2]);
+    const std::vector<double> eyeCoords = conf["General"]["eye"].as_double_tuple_or_die();
+    const Vector3D eyePoint = Vector3D::point(eyeCoords[0], eyeCoords[1], eyeCoords[2]);
 
     Figures3D figures;
     for (unsigned int i=0 ; i < nrFigs ; i++){
-        std::string fig_string = "Figure" + std::to_string(i);
+        const std::string fig_string = "Figure" + std::to_string(i);
 
         const double fig_scale = conf[fig_string]["scale"].as_double_or_die();
-        const double fig_rotateX = conf[fig_string]["rotateX"].as_double_or_die();
-        const double fig_rotateY = conf[fig_string]["rotateY"].as_double_or_die();
-        const double fig_rotateZ = conf[fig_string]["rotateZ"].as_double_or_die();
-        const std::vector<double> fig_center = conf[fig_string]["center"].as_double_tuple_or_die();
-
+        const double fig_rotateX = conf[fig_string]["rotateX"].as_double_or_die() * M_PI/180;
+        const double fig_rotateY = conf[fig_string]["rotateY"].as_double_or_die() * M_PI/180;
+        const double fig_rotateZ = conf[fig_string]["rotateZ"].as_double_or_die() * M_PI/180;
+        const std::vector<double> fig_centerCoords = conf[fig_string]["center"].as_double_tuple_or_die();
+        const Vector3D fig_centerPoint = Vector3D::point(fig_centerCoords[0], fig_centerCoords[1], fig_centerCoords[2]);
         const std::vector<double> fig_color = conf[fig_string]["color"].as_double_tuple_or_die();
-        Figure3D newFigure(Color(fig_color[0], fig_color[1], fig_color[2]));
 
-        unsigned int nrPoints = conf[fig_string]["nrPoints"].as_int_or_die();
+        Figure3D newFigure(Color(fig_color[0], fig_color[1], fig_color[2]));
+        const unsigned int nrPoints = conf[fig_string]["nrPoints"].as_int_or_die();
         for (unsigned int p=0 ; p < nrPoints ; p++){
             std::vector<double> coords = conf[fig_string]["point" + std::to_string(p)].as_double_tuple_or_die();
-            Vector3D point = Vector3D::vector(coords[0], coords[1], coords[2]);
-            newFigure.points.push_back(point);
+            newFigure.points.push_back(Vector3D::point(coords[0], coords[1], coords[2]));
         }
 
-        unsigned int nrLines = conf[fig_string]["nrLines"].as_int_or_die();
+        const unsigned int nrLines = conf[fig_string]["nrLines"].as_int_or_die();
         for (unsigned int l=0; l < nrLines ; l++){
             std::string lineString = "line" + std::to_string(l);
             std::vector<int> linePoints = conf[fig_string]["line" + std::to_string(l)].as_int_tuple_or_die();
             Face newFace;
-            for (int p : linePoints) newFace.push_back(p);
+            for (unsigned int p : linePoints) newFace.push_back(p);
             newFigure.faces.push_back(newFace);
         }
 
-//        applyTransform(newFigure, scale(fig_scale)
-//        *rotateX(fig_rotateX)*rotateY(fig_rotateY)*rotateZ(fig_rotateZ)
-//        *translate(fig_center[0], fig_center[1], fig_center[2])
-//        *eyePointTrans(eyePoint));
-
-        applyTransform(newFigure, scale(fig_scale));
-        applyTransform(newFigure, rotateX(fig_rotateX));
-        applyTransform(newFigure, rotateY(fig_rotateY));
-        applyTransform(newFigure, rotateZ(fig_rotateZ));
-        applyTransform(newFigure, translate(Vector3D::vector(fig_center[0], fig_center[1], fig_center[2])));
-        applyTransform(newFigure, eyePointTrans(eyePoint));
+        applyTransform(newFigure, scale(fig_scale)
+        *rotateX(fig_rotateX)*rotateY(fig_rotateY)*rotateZ(fig_rotateZ)
+        *translate(fig_centerPoint)
+        *eyePointTrans(eyePoint));
 
         figures.push_back(newFigure);
     }
+
     return draw2DLines(doProjection(figures), size, img::Color(lround(bgColor[0] * 255),
                                                                lround(bgColor[1] * 255),
                                                                lround(bgColor[2] * 255)));
